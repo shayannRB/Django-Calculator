d6538b02fc6d8bd05ac4ef98b8804e2a4059a47d
shayan <white1234dark@gmail.com>
2024-02-26 13:47:09 +0330

    'templates-got-created-and-view-worked-correctly'

--- main/views.py
+++ main/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
-# Create your views here.
+
+@csrf_protect
+def main(request):
+
+    number1  = request.POST.get('number1')
+    number2  = request.POST.get('number2')
+    opp  = request.POST.get('opperation')
+
+    
+    if opp == '*':
+        number1 = int(number1)
+        number2 = int(number2)
+
+        result = number1 * number2
+        return render(request, 'html2.html', context={'number' : str(result)}) 
+
+
+    if opp == '-':
+        number1 = int(number1)
+        number2 = int(number2)
+
+        result = number1 - number2
+        return render(request, 'html2.html', context={'number' : str(result)}) 
+
+    if opp == '+':
+        number1 = int(number1)
+        number2 = int(number2)
+
+        result = number1 + number2
+        return render(request, 'html2.html', context={'number' : str(result)}) 
+
+    if opp == '/':
+        number1 = int(number1)
+        number2 = int(number2)
+
+        result = number1 / number2
+        return render(request, 'html2.html', context={'number' : str(result)}) 
+    
+    
+    return render(request, 'html1.html', context={'number' : 'number'}) 
+    
